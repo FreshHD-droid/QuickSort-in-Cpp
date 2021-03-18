7024638fbb5bfe79652634e74f059e48d4f2f7ae
Marcu George <62952812+FreshHD-droid@users.noreply.github.com>
2021-03-19 00:17:22 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+int pozitionare(int stg, int dr, int arr[]) {
+    char mod = 'a';
+
+    if(stg == dr) {
+        return stg;
+    }else {
+        while(stg < dr) {
+
+            if(arr[stg] > arr[dr]) {
+
+                std:: swap(arr[stg], arr[dr]);
+                if(mod == 'a')mod = 'b';
+                else mod = 'a';
+
+            }
+
+            if(mod == 'a') {
+                dr--;
+            }else {
+                stg++;
+            }
+        }
+
+        return stg;
+    }
+}
+
+void QS(int stg, int dr, int ar[]) {
+    int k;
+
+    if(stg < dr) {
+        k = pozitionare(stg, dr, ar);
+        QS(stg, k - 1, ar);
+        QS(k + 1, dr, ar);
+    }
+}
+
+int main() {
+    int n, vec[10001];
+
+    std:: cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        std:: cin >> vec[i];
+    }
+
+    QS(1, n, vec);
+
+    for(int i = 1; i <= n; i++) {
+        std:: cout << vec[i] << ' ';
+    }
+
+    return 0;
+}
